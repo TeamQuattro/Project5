1d40a119b5d7268f4f0768618ef404d46ec5fa1a
Beckah Moscuzza <rhmoscuzza@gmail.com>
2017-08-30 19:02:27 -0400

    fixing vim issue

--- public/scripts/stopwatch.js
+++ public/scripts/stopwatch.js
@@ -1,64 +1,5 @@
 "use strict";
 
-<<<<<<< HEAD
-console.log("hello");
-
-function clock() {
-	var _this = this;
-
-	var time = 0;
-	var offset;
-	var interval;
-
-	function update() {
-		time += delta();
-		var timeFormatted = timeInMilliseconds(time);
-	}
-	function delta() {
-		var now = Date.now();
-		var timePassed = offset - now;
-		return timePassed;
-	}
-	function timeFormat(timeInMilliseconds) {
-		var time = new Date(timeInMilliseconds);
-		var minutes = time.getMinutes().toString();
-		var seconds = time.getSeconds().toString;
-		var milliseconds = time.getMilliseconds().toString;
-		if (minutes < 2) {
-			minutes = "0" + minutes;
-		}
-		if (seconds < 2) {
-			seconds = "0" + seconds;
-		}
-		if (milliseconds < 3) {
-			milliseconds = "0" + milliseconds;
-		}
-		return minutes + " : " + seconds + " : " + milliseconds;
-	}
-
-	this.started = false;
-	this.start = function () {
-		if (!_this.started) {
-			interval = setInterval(update, 100);
-			offset = Date.now();
-			_this.started = true;
-		}
-	};
-	this.stop = function () {
-		if (_this.started) {
-			clearInterval(interval);
-			interval = null;
-			_this.started = false;
-		}
-	};
-	this.reset = function () {
-		time = 0;
-	};
-}
-
-var watch = new clock();
-watch.start();
-=======
 // Stopwatch by Cathy Dutton 
 // https://codepen.io/cathydutton/pen/GBcvo
 
@@ -116,4 +57,3 @@ window.onload = function () {
     }
   }
 };
->>>>>>> 90702ed36fcacb0268aa8fb9e72e927f1f80a6f3
