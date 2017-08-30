8db624478ea82f7bf17b516b32e87656e05792d9
Richard Chin <ricch13@gmail.com>
2017-08-30 16:53:49 -0400

    both apis work

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -40,12 +40,13 @@ booksMovies.getMovieInfo = (userMovieChoice) => {
 		}
 	}).then((res) => {
 		console.log(res);
+		booksMovies.getData(userMovieChoice);
 	})
 }
 
 
 //get book information
-booksMovies.getData = () => {
+booksMovies.getData = (query) => {
 	$.ajax({
 		url: "https://www.googleapis.com/books/v1/volumes",
 		method: "GET",
@@ -53,74 +54,27 @@ booksMovies.getData = () => {
 		data: {
 			key: booksMovies.Bookkey,
 			format: "json",
-			q: "jurassic park"
+			q: query
 		}
 	}).then((res) => {
-		console.log("test is working");
+		console.log(res);
 		booksMovies.bookTitle = res.items[0].volumeInfo.title;
 		console.log(booksMovies.bookTitle);
-		console.log("whatever 2asdasd");
-	})
+	});
 };
 
 booksMovies.displayData = () => {
 };
 
-booksMovies.events = () => {
+booksMovies.events = () => { // <-- Events, ie on click / submit
 	$(".movieQuery").on("submit", handleSubmit)
 }
 
-$(booksMovies.init);
-booksMovies.init = () => {
+booksMovies.init = () => { // <-- INITIALIZING
 	console.log ("This works");
 	booksMovies.events();
 };
 
-$(() => {
+$(() => { // <-- DOCUMENT READY
   booksMovies.init();
 });
-
-
-// Notes on developer:
-// Use double quotes
-// Function style: Function expression (var function = () {})
-// ES6 best practices
-
-// PSEUDOCODE:
-// Input field where users input name of a movie (required)
-// (if no movie exists / can be found - do something - but we will have autocomplete to avoid this problem)
-// On submit: Searches the TMDB database for movies with the same title
-// Also look at the genres that the movie title is associated with
-// Return list of X (4) desired books
-// Connect to a book API (Google Books) to determine length of book (depends on API data)
-// Also display a paragraph (of 100 words) for user to time themselves reading
-// Timer on the designated paragraph. User can hit start to begin, and then stop once they finish reading it.
-// When click on start, timer starts (visible to user)
-// Option to reset timer
-// When click stop, timer stops - we store the number into a variable: UserTime.
-// Reading Speed: Measured in words per minute. Function: ReadingSPeed to Take number of words and divide by UserTime
-// Display Reading Speed: eading speed is displayed.
-
-// Input field: "How much time will you spend reading each day?" User inputs a number
-// Take that value and store it in a variable: UserFreeTime
-
-// Calculate how long it takes to read the book based on UserFreeTime and ReadingSpeed
-
-
-// readingSpeed = Words / minute
-// bookAvg = 250 words / page
-// totalPages = x pages / book (in pages)
-
-// using math, determine:
-// totalWords = totalPages * bookAvg (in words)
-
-// totalTimeToRead = totalWords / readingSpeed (in time)
-
-// freeTimePerDay = minutes
-
-// total days totalTimeToRead / freeTimePerDay
-
-// A paragraph is displayed to the user that shows the results
-// Also included is an average result (so user can compare their results to the average person)
-// (Disclaimer: Numbers may be slightly inaccurate)
-
